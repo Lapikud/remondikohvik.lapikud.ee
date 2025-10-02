40ab8c9ae3ac32abe04598ce26926f8eb234e191
Oluwatobiloba Promise Temitope <topsinoty@gmail.com>
2025-10-02 16:16:27 +0300

    setup icons, fonts and initial colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,26 @@
+/** @format */
+
+import { cn } from '@/lib/utils';
 import type { Metadata } from 'next';
+import { Lato, Syncopate } from 'next/font/google';
+import './global.css';
 
 export const metadata: Metadata = {
   title: 'Remondikohvik',
   description: 'Deploy your static Next.js site to GitHub Pages.',
 };
+
+const lato = Lato({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '700', '900'],
+  variable: '--font-lato',
+});
+
+const syncopate = Syncopate({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-syncopate',
+});
 
 export default function RootLayout({
   children,
@@ -12,7 +29,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={cn('antialiased', `${lato.variable} ${syncopate.variable}`)}>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+/** @format */
+
 export default function Home() {
   return (
     <main>
       <div>Next.js on GitHub Pages</div>
+      <p className="font-lato">some lato text</p>
+      <p className="font-syncopate">some syncopate text</p>
     </main>
   );
 }
