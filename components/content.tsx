739f7c9a53c283f3686815e00d15e5fc66c3ff67
Oluwatobiloba Promise Temitope <topsinoty@gmail.com>
2025-10-05 01:24:48 +0300

    attempt to make things look better

--- components/content.tsx
+++ components/content.tsx
@@ -8,7 +8,7 @@ export const ContentBox = ({
   children: React.ReactNode;
 }) => (
   <div className="w-full max-w-3xl flex flex-col gap-4 text-secondary-foreground">
-    <h2 className="font-syncopate text-3xl lg:text-6xl font-black bg-primary text-primary-foreground min-w-1/2 w-fit">
+    <h2 className="font-syncopate text-3xl lg:text-6xl font-black bg-primary text-primary-foreground min-w-1/2 w-fit px-4 py-2 flex items-center">
       {icon}
       {title}
     </h2>
