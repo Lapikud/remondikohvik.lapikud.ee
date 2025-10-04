c52ef44f53e8c59269954d4f57867225fad089ff
Uko <u8meil@gmail.com>
2025-10-04 19:12:17 +0300

    fixed not-good ai code

--- components/Dec.tsx
+++ components/Dec.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils';
 // named according to the Figma...need better name
 export const Dec = ({ direction }: { direction?: 'inverse' }) => (
   <div
-    className={cn('relative w-full h-full flex gap-7 z-0', {
+    className={cn('relative w-full h-full flex gap-7 z-3', {
       'flex-row-reverse': direction === 'inverse',
     })}>
     <div className="bg-primary w-19/72 h-full"></div>
