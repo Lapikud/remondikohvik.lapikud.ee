ae824551e112c1ebbe9921c77998ddd1d876306e
Oluwatobiloba Promise Temitope <topsinoty@gmail.com>
2025-10-05 01:12:48 +0300

    remade dec and added  info section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 /** @format */
 
 import { Hero } from '@/components/layout/hero';
+import { InfoSection } from '@/components/layout/info';
 
 export default function Home() {
   return (
     <main className="relative w-full h-full ">
       <Hero />
+      <InfoSection />
     </main>
   );
 }
