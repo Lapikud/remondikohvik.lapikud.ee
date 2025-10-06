d74e2ea8eeba56a6581cdc6b4d8cef286dbd5f99
SwagMuffin88 <katrilotamois@outlook.com>
2025-10-06 15:04:18 +0300

    Adjust margins and paddings, style the logos

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Logo} from "@/components/logo";
 import Image from "next/image";
-import { SiInstagram, SiFacebook } from "react-icons/si";
+import {SiInstagram, SiFacebook} from "react-icons/si";
+import {cn} from "@/lib/utils";
 
 const orgs = {
   Lapikud: [
@@ -19,47 +20,61 @@ const orgs = {
     "assets/Robotiklubi-logo.png",
     "",
   ],
+  "Kspace": [
+    "https://k-space.ee",
+    "assets/kspace-logo1.svg",
+    "--alpha-0",
+    "hidden lg:block"
+  ],
 };
 
 export const Footer = () => (
 
   <div
-    className="bg-secondary flex flex-col justify-center sm:justify-between px-6 py-8 md:px-12 md:py-16 gap-4 md:gap-8">
+    className="bg-secondary flex flex-col justify-center sm:justify-between px-6 py-8 md:px-12 md:py-16 gap-4 md:gap-8"
+  >
     <div
       id="logos"
-      className="flex md:items-center gap-8 md:gap-0 justify-between "
+      className="flex md:items-center gap-8 md:gap-0 justify-between flex-col md:flex-row"
     >
       <div className="flex flex-col items-start md:items-center">
         <Logo
           className="not-sm:hidden w-full h-full items-center"
           imgClassName="not-sm:hidden md:size-12 lg:size-12 mr-2"
-          textClassName="hidden lg:inline-block text-secondary-foreground lg:text-l"
-        />
+          textClassName="hidden lg:inline-block text-secondary-foreground lg:text-l"/>
       </div>
-      <div className="flex flex-row">
-        {Object.entries(orgs).map(([org, [link, src, color]]) => (
-          <div
-            key={org}
-            className="px-8 py-2 lg:py-2 w-full h-full lg:max-w-fit bg-black flex items-center justify-center"
-            style={color ? {backgroundColor: `var(${color})`} : undefined}
-          >
-            <a
-              href={link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="w-full h-full flex items-center justify-center relative z-20"
+
+      <div className="flex-wrap">
+        <div
+          className={cn(
+            "flex not-lg:flex-col lg:h-24 not-lg:bottom-0 w-full",
+            "h-auto *:h-12",
+            "lg:justify-start lg:gap-4",
+            "lg:px-25 lg:container"
+          )}>
+          {...Object.entries(orgs).map(([org, [link, src, color, display]]) => (
+            <div
+              key={org}
+              className={cn(`px-2 lg:py-2 h-full lg:max-w-fit flex items-center justify-center lg:rounded-full ${display}`)}
+              style={color ? {backgroundColor: `var(${color})`} : undefined}
             >
-              <Image
-                src={src}
-                alt={org}
-                width={240}
-                height={120}
-                className="max-w-full max-h-full object-contain sm:max-w-[220px] lg:max-w-[320px]"
-                style={{objectFit: "contain"}}
-              />
-            </a>
-          </div>
-        ))}
+              <a
+                href={link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="w-full h-full flex items-center justify-center relative z-20"
+              >
+                <Image
+                  src={src}
+                  alt={org}
+                  width={240}
+                  height={120}
+                  className={cn(`max-w-full max-h-full object-contain ${display ? "lg:max-w-fit" : "sm:max-w-[220px] lg:max-w-[320px]"}`)}
+                  style={{objectFit: "contain"}}/>
+              </a>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
 
@@ -67,8 +82,8 @@ export const Footer = () => (
       id="contact"
       className="flex flex-wrap text-accent"
     >
-      <div className="pr-12 ">
-        <h4 className="text-l font-syncopate font-bold">
+      <div className="pr-12 mb-4">
+        <h4 className="text-l font-syncopate font-bold mb-4">
           MTÜ Lapikud
         </h4>
 
@@ -83,11 +98,11 @@ export const Footer = () => (
       </div>
 
       <div className="pl-5 pr-5 ">
-        <h4 className="text-l font-syncopate font-bold">
+        <h4 className="text-l font-syncopate font-bold mb-4">
           Meie sotsiaalid
         </h4>
-        
-        <div className= "flex">
+
+        <div className="flex">
           <a
             href="https://www.instagram.com/lapikud"
             target="_blank"
@@ -97,10 +112,9 @@ export const Footer = () => (
             <SiInstagram
               title="Instagram"
               size={"2em"}
-              className="hover:text-primary transition"
-            />
+              className="hover:text-primary transition"/>
           </a>
-          
+
           <a
             href="https://www.facebook.com/Lapikud/"
             target="_blank"
@@ -110,17 +124,15 @@ export const Footer = () => (
             <SiFacebook
               title="Facebook"
               size={"2em"}
-              className="hover:text-primary transition" 
-            />
+              className="hover:text-primary transition"/>
           </a>
-          
+
         </div>
       </div>
     </div>
-
     <div className="flex items-center justify-center text-primary font-bold font-syncopate">
       © 2025 MTÜ Lapikud
     </div>
-
   </div>
+
 );
