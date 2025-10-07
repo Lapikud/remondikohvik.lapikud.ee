7d6fdd5ac0559854897bca348729b80fd17830fc
Oluwatobiloba Promise Temitope <topsinoty@gmail.com>
2025-10-07 12:00:21 +0300

    added kontakt section

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,18 @@ import { Hero } from "@/components/layout/hero";
 import { HistorySection } from "@/components/layout/history";
 import { InfoSection } from "@/components/layout/info";
 import { Navbar } from "@/components/layout/navbar";
-import {Footer} from "@/components/layout/footer";
+import { Footer } from "@/components/layout/footer";
+import { Kontakt } from "@/components/layout/kontakt";
 
 export default function Home() {
   return (
     <>
-      <Navbar   />
+      <Navbar />
       <main className="relative w-full h-full ">
         <Hero />
         <InfoSection />
         <HistorySection />
+        <Kontakt />
       </main>
       <Footer />
     </>
