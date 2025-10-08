8a01df9885af4822aafe344229afc8d1637746dd
placeholder <a@a.com>
2025-10-08 04:41:12 +0300

    Add aboutUs.tsx, about-us-1.jpg, about-us-2.jpg. Modify global.css, page.tsx.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 /** @format */
 
 import { Hero } from "@/components/layout/hero";
+import { AboutUs } from "@/components/layout/aboutUs";
 import { HistorySection } from "@/components/layout/history";
 import { InfoSection } from "@/components/layout/info";
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <main className="relative w-full h-full ">
       <Hero />
+      <AboutUs />
       <InfoSection />
       <HistorySection />
     </main>
