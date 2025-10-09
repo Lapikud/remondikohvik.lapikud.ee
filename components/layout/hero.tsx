0cc7196ec27c40639387ad1200cc33baefc7f67e
SwagMuffin88 <katrilotamois@outlook.com>
2025-10-09 10:59:05 +0300

    Remove md hover from map hyperlink

--- components/layout/hero.tsx
+++ components/layout/hero.tsx
@@ -1,9 +1,9 @@
 /** @format */
-import { Calendar, MapPin } from "lucide-react";
-import { Dec } from "@/components/Dec";
-import { cn } from "@/lib/utils";
+import {Calendar, MapPin} from "lucide-react";
+import {Dec} from "@/components/Dec";
+import {cn} from "@/lib/utils";
 import Image from "next/image";
-import { Logo } from "../logo";
+import {Logo} from "../logo";
 
 const orgs = {
   Lapikud: ["https://lapikud.ee", "assets/lapikud-logo.svg", "--primary"],
@@ -28,10 +28,10 @@ export const Hero = () => (
   <section className="w-full flex relative items-center justify-start md:pt-20">
     <div className="w-full h-full absolute top-0 left-0 overflow-hidden z-1">
       <div className="hidden xl:block w-2/5 h-full min-h-screen absolute right-0">
-        <Dec direction="inverse" />
+        <Dec direction="inverse"/>
       </div>
       <div className="w-full h-full relative">
-        <div className="z-2 w-full absolute top-0 left-0 h-full bg-secondary/67 backdrop-blur-[2px]" />
+        <div className="z-2 w-full absolute top-0 left-0 h-full bg-secondary/67 backdrop-blur-[2px]"/>
         <Image
           src="assets/hero.png"
           alt="hero"
@@ -42,7 +42,8 @@ export const Hero = () => (
     </div>
 
     {/* Main content */}
-    <div className="w-full h-full z-10 flex justify-between flex-col gap-4 lg:gap-8 relative text-secondary-foreground lg:py-48">
+    <div
+      className="w-full h-full z-10 flex justify-between flex-col gap-4 lg:gap-8 relative text-secondary-foreground lg:py-48">
       {/* Main section */}
       <div className="flex flex-col gap-4 lg:gap-8 px-8 lg:px-20 container py-32">
         <div className="flex flex-col sm:gap-4">
@@ -53,27 +54,28 @@ export const Hero = () => (
           />
           <h1 className="font-syncopate text-2xl md:text-4xl lg:text-6xl font-black">
             Kas midagi on katki?
-            <br />
+            <br/>
             Parandame koos!
           </h1>
         </div>
         <p className="md:text-xl">
           Elektroonikaseadmeid ei ole vaja ära visata! Loome koos jätkusuutlikku tulevikku!
         </p>
-        <div className="**:font-syncopate items-center px-4 max-w-full py-3 lg:px-10 lg:py-5 bg-primary w-max flex gap-4 lg:gap-8">
+        <div
+          className="**:font-syncopate items-center px-4 max-w-full py-3 lg:px-10 lg:py-5 bg-primary w-max flex gap-4 lg:gap-8">
           <div className="flex items-center gap-1 lg:gap-2.5 ">
-            <Calendar className="not-sm:size-3 -translate-y-0.25" />
+            <Calendar className="not-sm:size-3 -translate-y-0.25"/>
             <p className="font-bold text-white not-sm:text-[9px] md:text-xs lg:text-2xl">
               8. Nov 2025
             </p>
           </div>
           <div className="flex items-center gap-1 lg:gap-2.5 ">
-            <MapPin className="not-sm:size-3 -translate-y-0.25" />
+            <MapPin className="not-sm:size-3 -translate-y-0.25"/>
             <a
               href="https://maps.app.goo.gl/B77ZdAA7KW3k3mze6"
               target="_blank"
               rel="noopener noreferrer"
-              className="font-bold text-white md:hover:text-secondary-foreground not-sm:text-[9px] md:text-xs lg:text-2xl relative z-20"
+              className="font-bold text-white hover:text-secondary-foreground not-sm:text-[9px] md:text-xs lg:text-2xl relative z-20"
             >
               Taltech peamaja kohvik
             </a>
@@ -98,8 +100,8 @@ export const Hero = () => (
             key={org}
             className={cn(
               `h-[9ch] px-8 py-2 lg:py-2 w-full lg:max-w-fit bg-black flex items-center justify-center lg:rounded-full ${display} hover:opacity-75 transition-all`
-            )} 
-            style={color ? { backgroundColor: `var(${color})` } : undefined}
+            )}
+            style={color ? {backgroundColor: `var(${color})`} : undefined}
           >
             <a
               href={link}
@@ -120,7 +122,7 @@ export const Hero = () => (
                       : "sm:max-w-[220px] lg:max-w-[320px]"
                   }`
                 )}
-                style={{ objectFit: "contain" }}
+                style={{objectFit: "contain"}}
               />
             </a>
           </div>
